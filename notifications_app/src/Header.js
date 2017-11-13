29dc8a0659a0af3c3ca4f7f9e3ca59e0835bd1af
Erin <erinedawson@gmail.com>
2017-11-13 13:20:44 -0500

    add search icon to header

--- notifications_app/src/Header.js
+++ notifications_app/src/Header.js
@@ -6,13 +6,20 @@ class Header extends Component {
   render(){
     return (
       <div className="header">
-        {<h2>This is the header</h2>}
+        <div className="title">
+          {<h2>Timeline</h2>}
+        </div>
+
         <div className="navigation">
           {<ul>
             <a href="#"><li>Location 1</li></a>
             <a href="#"><li>Location 2</li></a>
             <a href="#"><li>Location 3</li></a>
           </ul>}
+        </div>
+
+        <div className="searchIcon">
+          <img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-128.png"/>
         </div>
       </div>
     )
